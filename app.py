5ca44506e9cf986d98ddafeb44f877fa13e569f8
pmstibbiacollege <pmstibbiacollege@rediffmail.com>
2024-09-12 00:53:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 app = Flask(__name__)
 
@@ -11,7 +12,7 @@ def check_link():
         with open('urls.txt', 'r') as file:
             link = file.readline().strip()
 
-        # Set up Selenium with BrowserStack
+        # Set up Chrome options
         options = Options()
         options.add_argument('--headless')
         options.add_argument('--disable-gpu')
@@ -21,31 +22,31 @@ def check_link():
         browserstack_username = 'YOUR_BROWSERSTACK_USERNAME'
         browserstack_access_key = 'YOUR_BROWSERSTACK_ACCESS_KEY'
 
-        capabilities = {
-            'os': 'Windows',
-            'os_version': '10',
-            'browser': 'Chrome',
-            'browser_version': 'latest',
-            'name': 'Flask App Test',
-            'browserstack.local': 'false',
-            'browserstack.selenium_version': '3.14.0'
-        }
+        # Desired capabilities for BrowserStack
+        capabilities = DesiredCapabilities.CHROME.copy()
+        capabilities['os'] = 'Windows'
+        capabilities['os_version'] = '10'
+        capabilities['browser'] = 'Chrome'
+        capabilities['browser_version'] = 'latest'
+        capabilities['name'] = 'Flask App Test'
+        capabilities['browserstack.local'] = 'false'
+        capabilities['browserstack.selenium_version'] = '3.14.0'
 
+        # Set up the Selenium WebDriver with BrowserStack
         driver = webdriver.Remote(
             command_executor=f'https://{browserstack_username}:{browserstack_access_key}@hub-cloud.browserstack.com/wd/hub',
-            desired_capabilities=capabilities,
-            options=options
+            options=options,
+            desired_capabilities=capabilities
         )
 
+        # Navigate to the link
         driver.get(link)
         driver.implicitly_wait(10)
+
+        # Get the second redirect URL
         second_redirect_url = driver.current_url
         driver.quit()
 
         return jsonify({'second_redirect_url': second_redirect_url})
-
+    
     except Exception as e:
-        return jsonify({'error': str(e)})
-
-if __name__ == '__main__':
-    app.run(debug=True)
