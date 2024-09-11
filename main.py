d12bb62d5b919ac81e61ff0139567a98f5d689e7
pmstibbiacollege <pmstibbiacollege@rediffmail.com>
2024-09-11 18:52:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import subprocess
 
 def get_chrome_version():
-result = subprocess.run(['google-chrome', '--version'], stdout=subprocess.PIPE)
-return result.stdout.decode('utf-8').strip()
+    result = subprocess.run(['google-chrome', '--version'], stdout=subprocess.PIPE)
+    return result.stdout.decode('utf-8').strip()
 
 chrome_version = get_chrome_version()
 print(f"Google Chrome version: {chrome_version}")
