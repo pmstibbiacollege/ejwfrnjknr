a976d435484ec57beae0494b0d9b8799200eb1c7
pmstibbiacollege <pmstibbiacollege@rediffmail.com>
2024-09-11 23:58:00 +0100

    Update browserstack_client.py

--- browserstack_client.py
+++ browserstack_client.py
@@ -7,15 +7,13 @@ from selenium.webdriver.support import expected_conditions as EC
 import os
 
 def fetch_redirect_url(url):
-    browserstack_username = os.getenv('zrh_8ZqMwbDvWoy')
-    browserstack_access_key = os.getenv('sbRQbrC7CqQfVoPzFK7c')
+    browserstack_username = os.getenv('BROWSERSTACK_USERNAME')
+    browserstack_access_key = os.getenv('BROWSERSTACK_ACCESS_KEY')
+
+    if not browserstack_username or not browserstack_access_key:
+        raise ValueError("BrowserStack credentials are missing")
 
     # BrowserStack configuration
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-
     capabilities = {
         'browserName': 'Chrome',
         'browserVersion': 'latest',
@@ -23,16 +21,18 @@ def fetch_redirect_url(url):
         'osVersion': '10',
         'name': 'Fetch Redirect URL Test'
     }
+    
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
 
     driver = webdriver.Remote(
-        command_executor='https://{}:{}@hub.browserstack.com/wd/hub'.format(browserstack_username, browserstack_access_key),
-        options=options
+        command_executor=f'https://{browserstack_username}:{browserstack_access_key}@hub.browserstack.com/wd/hub',
+        options=options,
+        desired_capabilities=capabilities
     )
-    
-    # Apply capabilities via options if necessary
-    driver.execute_cdp_cmd('Network.enable', {})
-    driver.execute_cdp_cmd('Network.setCacheDisabled', {'cacheDisabled': True})
-    
+
     try:
         driver.get(url)
         
