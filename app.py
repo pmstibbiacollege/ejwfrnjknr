580fa77f59809a9dee82874af49cbb738ceb6de0
pmstibbiacollege <pmstibbiacollege@rediffmail.com>
2024-09-12 00:38:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, request, jsonify
-import requests
+from flask import Flask, jsonify
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -7,45 +6,40 @@ app = Flask(__name__)
 
 @app.route('/check_link', methods=['POST'])
 def check_link():
-    # Read the link from urls.txt file
-    with open('urls.txt', 'r') as file:
-        link = file.readline().strip()
+with open('urls.txt', 'r') as file:
+link = file.readline().strip()
 
-    # Set up Selenium with BrowserStack
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--no-sandbox')
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
 
-    # BrowserStack credentials
-    browserstack_username = 'YOUR_BROWSERSTACK_USERNAME'
-    browserstack_access_key = 'YOUR_BROWSERSTACK_ACCESS_KEY'
+# BrowserStack credentials
+browserstack_username = 'YOUR_BROWSERSTACK_USERNAME'
+browserstack_access_key = 'YOUR_BROWSERSTACK_ACCESS_KEY'
 
-    capabilities = {
-        'os': 'Windows',
-        'os_version': '10',
-        'browser': 'Chrome',
-        'browser_version': 'latest',
-        'name': 'Flask App Test',
-        'browserstack.local': 'false',
-        'browserstack.selenium_version': '3.14.0'
-    }
+capabilities = {
+'os': 'Windows',
+'os_version': '10',
+'browser': 'Chrome',
+'browser_version': 'latest',
+'name': 'Flask App Test',
+'browserstack.local': 'false',
+'browserstack.selenium_version': '3.14.0'
+}
 
-    driver = webdriver.Remote(
-        command_executor=f'https://{browserstack_username}:{browserstack_access_key}@hub-cloud.browserstack.com/wd/hub',
-        desired_capabilities=capabilities,
-        options=options
-    )
+driver = webdriver.Remote(
+command_executor=f'https://{browserstack_username}:{browserstack_access_key}@hub-cloud.browserstack.com/wd/hub',
+desired_capabilities=capabilities,
+options=options
+)
 
-    driver.get(link)
-    # Wait for JavaScript to execute and redirects to complete
-    driver.implicitly_wait(10)
+driver.get(link)
+driver.implicitly_wait(10)
+second_redirect_url = driver.current_url
+driver.quit()
 
-    # Get the second redirect URL
-    second_redirect_url = driver.current_url
-    driver.quit()
-
-    return jsonify({'second_redirect_url': second_redirect_url})
+return jsonify({'second_redirect_url': second_redirect_url})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+app.run(debug=True)
