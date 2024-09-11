d19be5b4ac2f73c29d0fa6ba85c69ea439bf99d7
pmstibbiacollege <pmstibbiacollege@rediffmail.com>
2024-09-11 23:47:20 +0100

    Update browserstack_client.py

--- browserstack_client.py
+++ browserstack_client.py
@@ -7,10 +7,15 @@ from selenium.webdriver.support import expected_conditions as EC
 import os
 
 def fetch_redirect_url(url):
-    browserstack_username = os.getenv('zrh_8ZqMwbDvWoy')
-    browserstack_access_key = os.getenv('sbRQbrC7CqQfVoPzFK7c')
+    browserstack_username = os.getenv('BROWSERSTACK_USERNAME')
+    browserstack_access_key = os.getenv('BROWSERSTACK_ACCESS_KEY')
 
     # BrowserStack configuration
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
     capabilities = {
         'browserName': 'Chrome',
         'browserVersion': 'latest',
@@ -18,18 +23,16 @@ def fetch_redirect_url(url):
         'osVersion': '10',
         'name': 'Fetch Redirect URL Test'
     }
-    
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
 
     driver = webdriver.Remote(
         command_executor='https://{}:{}@hub.browserstack.com/wd/hub'.format(browserstack_username, browserstack_access_key),
-        options=options,
-        capabilities=capabilities  # Updated to capabilities
+        options=options
     )
-
+    
+    # Apply capabilities via options if necessary
+    driver.execute_cdp_cmd('Network.enable', {})
+    driver.execute_cdp_cmd('Network.setCacheDisabled', {'cacheDisabled': True})
+    
     try:
         driver.get(url)
         
