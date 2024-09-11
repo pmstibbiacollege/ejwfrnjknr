b4d3014d3da1d65611e7842ac970f4161529bae6
pmstibbiacollege <pmstibbiacollege@rediffmail.com>
2024-09-12 00:28:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,51 @@
-from flask import Flask, jsonify
-import os
-from browserstack_client import fetch_redirect_url
+from flask import Flask, request, jsonify
+import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    # Read the URL from a text file
+@app.route('/check_link', methods=['POST'])
+def check_link():
+    # Read the link from urls.txt file
     with open('urls.txt', 'r') as file:
-        url = file.readline().strip()
-    
-    # Fetch the second redirect URL using BrowserStack
-    redirect_url = fetch_redirect_url(url)
-    
-    return jsonify({'redirect_url': redirect_url})
+        link = file.readline().strip()
+
+    # Set up Selenium with BrowserStack
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+
+    # BrowserStack credentials
+    browserstack_username = 'YOUR_BROWSERSTACK_USERNAME'
+    browserstack_access_key = 'YOUR_BROWSERSTACK_ACCESS_KEY'
+
+    capabilities = {
+        'os': 'Windows',
+        'os_version': '10',
+        'browser': 'Chrome',
+        'browser_version': 'latest',
+        'name': 'Flask App Test',
+        'browserstack.local': 'false',
+        'browserstack.selenium_version': '3.14.0'
+    }
+
+    driver = webdriver.Remote(
+        command_executor=f'https://{browserstack_username}:{browserstack_access_key}@hub-cloud.browserstack.com/wd/hub',
+        desired_capabilities=capabilities,
+        options=options
+    )
+
+    driver.get(link)
+    # Wait for JavaScript to execute and redirects to complete
+    driver.implicitly_wait(10)
+
+    # Get the second redirect URL
+    second_redirect_url = driver.current_url
+    driver.quit()
+
+    return jsonify({'second_redirect_url': second_redirect_url})
 
 if __name__ == '__main__':
     app.run(debug=True)
