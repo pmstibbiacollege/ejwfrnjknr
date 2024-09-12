1cb0588d8f0f2212d5ad513b767e22ba4eb0558f
pmstibbiacollege <pmstibbiacollege@rediffmail.com>
2024-09-12 01:16:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,9 @@ app = Flask(__name__)
 @app.route('/check_link', methods=['POST'])
 def check_link():
     try:
-        # Read the link from urls.txt file
         with open('urls.txt', 'r') as file:
             link = file.readline().strip()
 
-        # Set up Chrome options
         options = Options()
         options.add_argument('--headless')
         options.add_argument('--disable-gpu')
@@ -21,7 +19,6 @@ def check_link():
         browserstack_username = 'YOUR_BROWSERSTACK_USERNAME'
         browserstack_access_key = 'YOUR_BROWSERSTACK_ACCESS_KEY'
 
-        # BrowserStack capabilities
         capabilities = {
             'bstack:options': {
                 'os': 'Windows',
@@ -36,23 +33,17 @@ def check_link():
             }
         }
 
-        # Initialize the WebDriver with BrowserStack
         driver = webdriver.Remote(
             command_executor=f'https://{browserstack_username}:{browserstack_access_key}@hub-cloud.browserstack.com/wd/hub',
-            options=options,
-            desired_capabilities=capabilities
+            options=options
         )
 
-        # Navigate to the link
         driver.get(link)
         driver.implicitly_wait(10)
-
-        # Get the second redirect URL
         second_redirect_url = driver.current_url
         driver.quit()
 
         return jsonify({'second_redirect_url': second_redirect_url})
-    
     except Exception as e:
         return jsonify({'error': str(e)})
 
